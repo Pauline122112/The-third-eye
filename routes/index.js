408f92dec0020248ff965288d415c468b1f09be5
Pauline122112 <pauline.design007@gmail.com>
2017-03-27 23:13:04 +1300

    added slideshow carousel

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var db = require('../db')
 
 module.exports = {
   get: get,
-  getForm: getForm
+  getForm: getForm,
+  // plusSlides: plusSlides,
+  // currentSlide: currentSlide,
+  // showSlides: showSlides
 }
 
 function get (req, res) {
@@ -16,3 +19,30 @@ function getForm (req, res) {
 
   res.render("sellerForm.hbs")
 }
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
--- seeds/users.js
+++ seeds/users.js
@@ -2,7 +2,7 @@ exports.seed = function (knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        
+
       ]);
     });
 };
